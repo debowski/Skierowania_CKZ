96780137e5a3f733b6883bb23dd96e873ef8c85f
debowski <piotr.debowski@zseiulaziska.pl>
2024-10-09 15:11:51 +0200

    update v0.38

--- kompilacja.bat
+++ kompilacja.bat
@@ -1,1 +1,1 @@
-python -m PyInstaller  --add-data ".\src\zawody.json;." --add-data ".\src\Data;Data" --add-data ".\src\Szablony;Szablony"  .\src\app_v37.py
+python -m PyInstaller  --add-data ".\src\zawody.json;." --add-data ".\src\Data;Data" --add-data ".\src\Szablony;Szablony"  .\src\app_v38.py
